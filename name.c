5e2951e63d5ad3b9608148d4cfefe1ff47db77b6
srikanthpragada <srikanthpragada@gmail.com>
2021-10-28 18:41:14 +0530

    28-OCT

---
+++ name.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main()
+{
+   printf("Srikanth Technologies\n");
+   printf("9059057000");
+}
---
+++ square.c
@@ -0,0 +1,16 @@
+// Program to calculate square of the given number
+
+#include <stdio.h>
+
+void main()
+{
+  int num, result;
+
+      printf("Enter a number :");
+      scanf("%d", &num);
+
+      result = num * num;
+
+      printf("Square = %d", result);
+
+}
