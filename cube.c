89d8aef36fb613626f61cf6a8d7b57d491c7c6ee
srikanthpragada <srikanthpragada@gmail.com>
2021-10-28 18:46:15 +0530

    28-OCT

---
+++ cube.c
@@ -0,0 +1,15 @@
+// Program to calculate cube
+
+#include<stdio.h>
+
+void main()
+{
+ int num, result;  // declare variables
+
+   printf("Enter a number :");
+   scanf("%d",&num);
+
+   result= num * num * num;
+
+   printf("Cube of %d is %d",num, result);
+ }
