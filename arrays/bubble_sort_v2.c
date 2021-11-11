3354f939d5bb64cc567a941d1180f8615a6f9c81
srikanthpragada <srikanthpragada@gmail.com>
2021-11-11 18:56:29 +0530

    11-NOV

---
+++ arrays/bubble_sort_v2.c
@@ -0,0 +1,48 @@
+// Create array of random numbers and sort using bubble sort
+
+#include <stdio.h>
+#include <stdlib.h>
+#define  SIZE 10
+
+void main()
+{
+  int a[SIZE];
+  int i,j,k,temp, swapped = 0;
+
+      srand(time(0));
+      printf("Original Array : ");
+      for(i = 0; i < SIZE; i ++)
+      {
+          a[i] = rand() % 100;
+          printf("%5d", a[i]);
+      }
+
+      // Bubble sort
+      for(i = 1; i < SIZE; i ++)
+      {
+         swapped = 0;
+         for(j = 0;  j < SIZE - i; j ++)
+         {
+             if (a[j] > a[j+1])
+             {
+                 temp = a[j];
+                 a[j] = a[j+1];
+                 a[j+1] = temp;
+                 swapped = 1;
+             }
+         }
+
+         // print after sorting array
+         printf("\nAfter round %d : ", i);
+         for(k = 0; k < SIZE; k ++)
+         {
+              printf("%5d", a[k]);
+         }
+
+         if(!swapped)
+            break;
+
+      }
+
+
+}
